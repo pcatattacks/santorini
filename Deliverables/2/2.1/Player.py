c08b0ecc02453fecd5b78fa49aba4fd0ef4bbb05
Pranav Dhingra <pranav.dhingra@gmail.com>
2018-10-09 19:23:34 -0500

    Added contracts as assertions and testing for player

--- Deliverables/2/2.1/Player.py
+++ Deliverables/2/2.1/Player.py
@@ -3,15 +3,28 @@ import random
 
 class Player(object):
 
-    @staticmethod
-    def start_game():
-        return random.randint(0, 11)
+    def __init__(self):
+        self.started = False
+        self.made_choice = False
 
-    @staticmethod
-    def keep_number():
-        return random.choice((True, False))
+    def start_game(self):
+        assert not self.started, "Can't call player.start_game() more than once until player.end_game() is called."
+        self.started = True
+        # return random.randint(0, 11)
+        return 9  # only for testing
 
-    @staticmethod
-    def end_game(won):
+    def keep_number(self):
+        assert self.started, "Can't call player.keep_number() until player.start_game() is called."
+        assert not self.made_choice, "Can't call player.keep_number() more than \
+                                        once after player.start_game() is called."
+        self.made_choice = True
+        # return random.choice((True, False))
+        return False  # only for testing
+
+    def end_game(self, won):
+        assert self.started, "Can't call player.end_game() until player.start_game() is called."
+        assert self.made_choice, "Can't call player.end_game() until player.keep_number() is called."
         assert type(won) is bool
+        self.started = False
+        self.made_choice = False
 
--- Deliverables/2/2.1/main.py
+++ Deliverables/2/2.1/main.py
@@ -4,14 +4,15 @@ from JsonParser import take_input, parse_json
 
 
 def main():
+    player = Player()
     command_list = parse_json(take_input())
     for command_obj in command_list:
-        operation = getattr(Player, command_obj['value']['operation-name'])
+        operation = getattr(player, command_obj['value']['operation-name'])
         arguments = []
         for i in range(1, len(command_obj['value'])):
             arguments.append(command_obj['value']['operation-argument{}'.format(i)])
         result = operation(*arguments)
-        print(json.dumps([operation(*arguments)])[1:-1])
+        print(json.dumps([result])[1:-1])
 
 
 if __name__ == '__main__':
