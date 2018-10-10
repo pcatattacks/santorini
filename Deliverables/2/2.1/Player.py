b470e4ecabb4e2ccd6e3183852dd9c97aac97c5b
Pranav Dhingra <pranav.dhingra@gmail.com>
2018-10-09 19:47:37 -0500

    Changed assertions to ContractErrors

--- Deliverables/2/2.1/Player.py
+++ Deliverables/2/2.1/Player.py
@@ -1,6 +1,11 @@
 import random
 
 
+class ContractError(Exception):
+    """Custom exception for violating contracts."""
+    pass
+
+
 class Player(object):
 
     def __init__(self):
@@ -8,23 +13,29 @@ class Player(object):
         self.made_choice = False
 
     def start_game(self):
-        assert not self.started, "Can't call player.start_game() more than once until player.end_game() is called."
+        if self.started:
+            raise ContractError("Can't call player.start_game() more than once until player.end_game() is called.")
         self.started = True
         # return random.randint(0, 11)
         return 9  # only for testing
 
     def keep_number(self):
-        assert self.started, "Can't call player.keep_number() until player.start_game() is called."
-        assert not self.made_choice, "Can't call player.keep_number() more than \
-                                        once after player.start_game() is called."
+        if not self.started:
+            raise ContractError("Can't call player.keep_number() until player.start_game() is called.")
+        elif self.made_choice:
+            raise ContractError("Can't call player.keep_number() more than \
+                                        once after player.start_game() is called.")
         self.made_choice = True
         # return random.choice((True, False))
         return False  # only for testing
 
     def end_game(self, won):
-        assert self.started, "Can't call player.end_game() until player.start_game() is called."
-        assert self.made_choice, "Can't call player.end_game() until player.keep_number() is called."
-        assert type(won) is bool
+        if not self.started:
+            raise ContractError("Can't call player.end_game() until player.start_game() is called.")
+        elif not self.made_choice:
+            raise ContractError("Can't call player.end_game() until player.keep_number() is called.")
+        elif type(won) is not bool:
+            raise TypeError("Player.end_game() only accepts one boolean argument.")
         self.started = False
         self.made_choice = False
 
