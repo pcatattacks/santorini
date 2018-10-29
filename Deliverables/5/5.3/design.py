825bbae57bae88bb9f0f1c7882ee95af1f85269d
Pranav Dhingra <pranav.dhingra@gmail.com>
2018-10-29 15:46:47 -0500

    started design.py

---
+++ Deliverables/5/5.3/design.py
@@ -0,0 +1,35 @@
+# from Player import Player
+# from RuleChecker import RuleChecker
+
+
+class Referee:
+    """
+
+    """
+    def __init__(self, player1, player2):
+        """
+
+        :param Player player1:
+        :param Player player2:
+        """
+        pass
+
+    def start_game(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def check_play(self, color, *play):
+        """
+
+        :param color:
+        :param play:
+        :return:
+        """
+        pass
+
+    # def notify_player(self, ):
+    #    pass
+
